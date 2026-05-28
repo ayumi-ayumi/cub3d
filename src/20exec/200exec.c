c942a5e8931eb00fa2f7bcc1ee7ec57b74a3bbb9
mmClara42 <chagen@c4b2c4.42berlin.de>
2026-05-28 16:57:14 +0200

    EDIT exec.c: added gamedata init

--- src/20exec/200exec.c
+++ src/20exec/200exec.c
@@ -8,12 +8,35 @@ static void	cleanup_exec(t_game *game, t_exec *exec)
 		free_mlx(game, exec);
 }
 
+// static int	turn_vec(double vec[2], int angle)
+// {
+// }
+
+static int	init_play_data(t_game *game, t_play *play)
+{
+	play.pos[0] = calc_pos(game, game.map.width, game.map.start_pos.col);
+	play.pos[1] = calc_pos(game, game.map.height, game.map.start_pos.row);
+	if (game.map.start_orientation == 'N')
+		play.dir = (t_vec){0, 1};
+	if (game.map.start_orientation == 'S')
+		play.dir = (t_vec){0, 1};
+	if (game.map.start_orientation == 'W')
+		play.dir = (t_vec){1, 0};
+	if (game.map.start_orientation == 'E')
+		play.dir = (t_vec){-1, 0};
+}
+
+/*setup of screen and start game loop*/
 int	execution(t_game *game)
 {
 	t_exec	exec;
+	t_play	play;
 
-	if (init_mlx(game, &exec) == FAIL || !game->mlx)
+	if (init_mlx(game, &exec) == FAIL || game->mlx == NULL
+				|| init_play_data(game, &play!) == FAIL)
 		return (cleanup_exec(game, &exec), FAIL);
-	cleanup_exec(game, &exec);
-	return (SUCCESS);
+	exec.play = play;
+
+	raycast(game, exec);
+	return (cleanup_exec(game, &exec), SUCCESS);
 }
