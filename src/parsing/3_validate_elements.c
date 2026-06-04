cc3f13f8864f85424e9edc5bd4eccffc104857c5
Ayumi Sato <asato@c4b1c1.42berlin.de>
2026-06-04 16:36:32 +0200

    ADD validate_elements.c: added strtrim for config lines

---
+++ src/parsing/3_validate_elements.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   3_validate_elements.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asato <asato@student.42berlin.de>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/25 17:26:43 by asato             #+#    #+#             */
+/*   Updated: 2026/06/04 16:04:27 by asato            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include "cub3d.h"
+#include "parser.h"
+#include "libft.h"
+#include <unistd.h>
+
+static int	validate_prefix(t_game *game)
+{
+	int	i;
+	int	found;
+
+	i = 0;
+	found = 0;
+	while (i < 6)
+	{
+		if (ft_strncmp(game->file_contents[i], "NO", 2) == 0)
+			found++;
+		if (ft_strncmp(game->file_contents[i], "SO", 2) == 0)
+			found++;
+		if (ft_strncmp(game->file_contents[i], "WE", 2) == 0)
+			found++;
+		if (ft_strncmp(game->file_contents[i], "EA", 2) == 0)
+			found++;
+		if (ft_strncmp(game->file_contents[i], "F ", 2) == 0)
+			found++;
+		if (ft_strncmp(game->file_contents[i], "C ", 2) == 0)
+			found++;
+		i++;
+	}
+	if (found != 6)
+		return (0);
+	return (1);
+}
+
+static int	trim_space_in_line(t_game *game)
+{
+	int		i;
+	char	*temp;
+
+	i = 0;
+	while (i < 6)
+	{
+		temp = ft_strtrim(game->file_contents[i], " ");
+		if (!temp)
+			return (0);
+		free(game->file_contents[i]);
+		game->file_contents[i] = temp;
+		if (!game->file_contents[i])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static int	validate_config(t_game *game)
+{
+	if (!trim_space_in_line(game))
+		return (0);
+	if (!validate_prefix(game))
+		return (0);
+	game->config.dir_path = ft_calloc(5, sizeof(char *));
+	if (!game->config.dir_path)
+		return (0);
+	if (!extract_dir_config(game))
+		return (free_dir_parh(game->config.dir_path), 0);
+	if (!extract_rgb_config(game))
+		return (0);
+	return (1);
+}
+
+int	exract_elements(t_game *game)
+{
+	if (!validate_config(game))
+		return (print_error("Imcopmete config data\n"), 0);
+	game->map.height = game->map.height - 6 + 1;
+	if (!extract_map(game))
+		return (0);
+	return (1);
+}
