7fe8640a086169563d5fa2465a1417d6b6c02542
mmClara42 <chagen@c4c1c2.42berlin.de>
2026-06-05 17:45:11 +0200

    EDIT draw.c: moved put_pixel to its own file as it will be used in ceveral files

--- src/5_render/1_draw.c
+++ src/5_render/1_draw.c
@@ -1,16 +1,5 @@
-#include "cub3d"
+#include "cub3d.h"
 #include "exec.h"
-
-static void	put_pixel(t_data *img, int x, int y, int colour)
-{
-	int	pixel;
-
-	pixel = y * (img->line_length / 4) + x;
-	img->addr[pixel] = colour;
-}
-
-static void	put_wall(t_exec *exec, int cam_x, int *i)
-
 /*draw screen col from top down*/
 void	draw_line(t_exec *exec, int cam_x)
 {
