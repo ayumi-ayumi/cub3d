6a0d39e95c7eda7a6bf497a09ee97841843e13e5
mmClara42 <chagen@c4b9c1.42berlin.de>
2026-06-12 13:33:25 +0200

    ADD movements.c and turns.c and listen.c added listening function and empty movement and turn functions

---
+++ src/5_move/3_movements.c
@@ -0,0 +1,10 @@
+#include "cub3d.h"
+#include "exec.h"
+
+
+/*moves with WDSA keys*/
+void	move_on(t_play *play)
+void	move_back(t_play *play)
+void	move_left(t_play *play)
+void	move_right(t_play *play)
+
