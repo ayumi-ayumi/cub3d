42d6c04bb8afd4c8718c17744ed3bd6f3ee8975b
teateecha <218916862+teateecha@users.noreply.github.com>
2026-06-13 14:53:55 +0200

    EDIT move.c: added recalculating and drawing after each hook

--- src/2_exec/4_move.c
+++ src/2_exec/4_move.c
@@ -26,7 +26,7 @@ static void	move_side(t_play *play, char dir)
 	play->pos.y = play->pos.y + (new_dir.y * play->move.move_speed);
 }
 
-
+/*changes pos and dir according to key and recalculates and sends img to win*/
 static void	handle_key_event(int	keycode, t_game *game)
 {
 	t_play	play;
@@ -46,6 +46,8 @@ static void	handle_key_event(int	keycode, t_game *game)
 		play.dir = turn_vec(play.dir, M_PI_2);
 	if(keycode == XK_Right)
 		play.dir = turn_vec(play.dir, 3 * M_PI_2);
+	raycast(game, &game->exec);
+	mlx_put_image_to_window(game->mlx, game->win, game->exec.screen.img, 0, 0);
 }
 
 
