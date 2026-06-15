5c18f4eebbd060feede4bf7118debf8196555b65
mmClara42 <chagen@c4b4c1.42berlin.de>
2026-06-15 18:41:24 +0200

    EDIT put_pixel and typecasted to unsigned int from char

--- src/4_raycast/4_put_pixel.c
+++ src/4_raycast/4_put_pixel.c
@@ -5,5 +5,5 @@ void	put_pixel(t_data *img, int x, int y, unsigned int color)
 	int	pixel;
 
 	pixel = y * (img->line_length / 4) + x;
-	img->addr[pixel] = color;
+	((unsigned int *)img->addr)[pixel] = color;
 }
