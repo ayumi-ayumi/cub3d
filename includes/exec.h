9b6a02eed17200e46b897959a7ea8d5f005da8dd
teateecha <218916862+teateecha@users.noreply.github.com>
2026-06-16 11:09:51 +0200

    EDIT exec.h: chenged for very small window

--- includes/exec.h
+++ includes/exec.h
@@ -1,8 +1,8 @@
 #ifndef EXEC_H
 # define EXEC_H
 
-# define SCREEN_HEIGHT 1480
-# define SCREEN_WIDTH 1640
+# define SCREEN_HEIGHT 48
+# define SCREEN_WIDTH 64
 # define TEXTURE_WIDTH 32
 # define TEXTURE_HEIGHT 32
 # define NORTH_SOUTH 1
