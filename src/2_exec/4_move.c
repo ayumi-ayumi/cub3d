7c107d8848684f10f969625731164aadbb2f306d
teateecha <218916862+teateecha@users.noreply.github.com>
2026-06-17 13:56:59 +0200

    EDIT move: changed movement functions to check for wall collitions

--- src/2_exec/4_move.c
+++ src/2_exec/4_move.c
@@ -6,28 +6,34 @@
 #include <stdio.h>
 
 /*moves with WDSA keys*/
-static void	move_back_forth(t_play *play, int dir)
+static void	move_back_forth(t_game *game, t_play *play, int dir)
 {
+	t_vec new;
+
 	// play->pos.x = play->pos.x + dir * (play->dir.x * play->move.steplength);
 	// play->pos.y = play->pos.y + dir * (play->dir.y * play->move.steplength);
-	play->pos.x = play->pos.x + dir * (play->dir.x * MOVESPEED);
-	play->pos.y = play->pos.y + dir * (play->dir.y * MOVESPEED);
+	new.x = play->pos.x + dir * (play->dir.x * MOVESPEED);
+	new.y = play->pos.y + dir * (play->dir.y * MOVESPEED);
+	if (game->map.grid[(int)play->pos.y][(int)new.x] == '0')
+		play->pos.x = new.x;
+	if (game->map.grid[(int)new.y][(int)play->pos.x] == '0')
+		play->pos.y = new.y;
 }
 
-static void	move_side(t_play *play, char dir)
+static void	move_side(t_game *game, t_play *play, double angle)
 {
 	t_vec	new_dir;
-	char	factor;
+	t_vec	new;
 
-	if (dir == 'l')
-		factor = 1;
-	if (dir == 'r')
-		factor = -1;
-	new_dir = turn_vec(play->dir, (double)factor * M_PI_2);
+	new_dir = turn_vec(play->dir, angle);
 	// play->pos.x = play->pos.x + (new_dir.x * play->move.steplength);
 	// play->pos.y = play->pos.y + (new_dir.y * play->move.steplength);
-	play->pos.x = play->pos.x + (new_dir.x * MOVESPEED);
-	play->pos.y = play->pos.y + (new_dir.y * MOVESPEED);
+	new.x = play->pos.x + (new_dir.x * MOVESPEED);
+	new.y = play->pos.y + (new_dir.y * MOVESPEED);
+	if (game->map.grid[(int)play->pos.y][(int)new.x] == '0')
+		play->pos.x = new.x;
+	if (game->map.grid[(int)new.y][(int)play->pos.x] == '0')
+		play->pos.y = new.y;
 }
 
 /*changes pos and dir according to key and recalculates and sends img to win*/
@@ -39,13 +45,13 @@ static void	handle_key_event(int	keycode, t_game *game)
 		cleanup_and_exit(game);
 	}
 	if (keycode == XK_w || keycode == XK_Up)
-		move_back_forth(&game->exec.play, 1);
+		move_back_forth(game, &game->exec.play, 1);
 	if (keycode == XK_s || keycode == XK_Down)
-		move_back_forth(&game->exec.play, -1);
+		move_back_forth(game, &game->exec.play, -1);
 	if (keycode == XK_a)
-		move_side(&game->exec.play, 'l');
+		move_side(game, &game->exec.play, M_PI_2);
 	if (keycode == XK_d)
-		move_side(&game->exec.play, 'r');
+		move_side(game, &game->exec.play, 3 * M_PI_2);
 	if(keycode == XK_Left)
 		game->exec.play.dir = turn_vec(game->exec.play.dir, M_PI_2 * ROTSPEED);
 	if(keycode == XK_Right)
