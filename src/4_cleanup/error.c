eed0c995bccf0262d12ab12bd9cb45ae884c9623
Ayumi Sato <asato@c1b4c1.42berlin.de>
2026-07-02 15:56:44 +0200

    CREATE ./4_cleanup & RENAME files

--- src/4_cleanup/error.c
+++ src/4_cleanup/error.c
@@ -6,7 +6,7 @@
 /*   By: asato <asato@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/25 17:43:43 by asato             #+#    #+#             */
-/*   Updated: 2026/06/30 19:36:44 by asato            ###   ########.fr       */
+/*   Updated: 2026/07/02 15:56:15 by asato            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	error_and_exit(char *error)
 	exit(0);/*@Ayumi: i think we should exit with a FAIL = 1 here not with SUCCESS = 0 */
 }
 
-void	print_error(char *error) // it can be stored in global utils
+void	print_error(char *error)
 {
 	printf("Error: %s\n", error);
 }
